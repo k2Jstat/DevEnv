04659ad416a4710245e0dc96fbe7517739d68eb1
k2Jstat <34390404+k2Jstat@users.noreply.github.com>
2023-06-13 19:42:08 +0900

    Delete ins_torch.sh

--- docker/rocker/sptime/dockerfile/ins_torch.sh
+++
@@ -1,4 +0,0 @@
-install2.r --error --skipmissing --skipinstalled torch luz torchvision torchaudio topicmodels.etm word2vec doc2vec textplot innsight
-R -q -e 'torch::install_torch()'
-#R -q -e 'remotes::install_github("mlverse/luz")'
-#R -q -e 'remotes::install_github("mlverse/torchvision@main")'
