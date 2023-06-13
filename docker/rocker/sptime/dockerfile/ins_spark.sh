364876c5ffecd16b458125029dfec2aca19aaff7
k2Jstat <34390404+k2Jstat@users.noreply.github.com>
2023-06-13 19:41:42 +0900

    Delete ins_spark.sh

--- docker/rocker/sptime/dockerfile/ins_spark.sh
+++
@@ -1,5 +0,0 @@
-R -q -e 'devtools::install_github("rstudio/sparklyr")'
-#RUN R -q -e 'sparklyr::spark_install("3.3.1")'
-R -q -e 'sparklyr::spark_install()'
-R -q -e 'devtools::install_github("rstudio/sparkxgb")'
-install2.r --error --skipmissing --skipinstalled apache.sedona arrow sparklyr.nested corrr dbplot sparkextension sparktf tfdatasets variantspark geospark
