d839c0e709d9e6a24aede93b30916fbf4b15b287
k2Jstat <34390404+k2Jstat@users.noreply.github.com>
2023-06-13 19:41:55 +0900

    Delete ins_tidy_gg.sh

--- docker/rocker/sptime/dockerfile/ins_tidy_gg.sh
+++
@@ -1,5 +0,0 @@
-#tidy series
-install2.r --error --skipmissing --skipinstalled tidytext tidymodels tidyposterior spatialsample discrim bayesian bayesmodels DBI tidygeocoder
-
-# ggplot
-install2.r --error --skipmissing --skipinstalled GGally ggmosaic ggquiver iterLap ggcorrplot ggdag ggtree ggtreeExtra ggfortify ggparty ggthemes
