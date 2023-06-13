01025cdfe711ff6cc16d78c368e4cd026be0fce9
k2Jstat <34390404+k2Jstat@users.noreply.github.com>
2023-06-13 19:41:20 +0900

    Delete ins_doc.sh

--- docker/rocker/sptime/dockerfile/ins_doc.sh
+++
@@ -1,3 +0,0 @@
-install2.r --error --skipmissing --skipinstalled rticles rmarkdown knitr DT flexdashboard htmlwidgets bookdown tableone knitr rmarkdown sessioninfo
-install2.r --error --skipmissing --skipinstalled tinytex latex2exp kableExtra distill equatiomatic DiagrammerR xfun kable magick
-R -q -e 'tinytex::install_tinytex(force = TRUE);tinytex::tlmgr_install("ipaex")'
